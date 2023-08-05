9ff7e0d9ec30484dfa78ca371d28ec3471e0fe80
saklani <50768838+saklani@users.noreply.github.com>
2023-08-05 16:09:40 +0530

    add return types for stack

--- src/stack.py
+++ src/stack.py
@@ -3,14 +3,14 @@ class Stack:
         self.stack = []
         self.max_depth = max_depth
 
-    def pop(self):
+    def pop(self) -> int:
         if len(self.stack) == 0:
             raise StackUnderflow()
         top = self.stack[-1]
         self.stack.pop()
         return top
 
-    def push(self, value: int):
+    def push(self, value: int) -> None:
         if len(self.stack) == 1024:
             raise StackOverflow()
         elif value < 0 or value > 2**256-1:
